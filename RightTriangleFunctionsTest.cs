b1bcd653bf5b67157d489b4b8eb02986624909ae
mrsWolowitz <mrsWolowitz@users.noreply.github.com>
2016-09-12 07:36:24 +0300

    Добавлены файлы

---
+++ RightTriangleFunctionsTest.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+
+namespace RightTriangleFunctionsTest
+{
+    using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+    [TestClass]
+    public class RightTriangleFunctionsTest
+    {
+        [TestMethod]
+        public void Should_return_2_04_When_cathet1_1_2_and_cathet2_3_4_and_hypotenuse_5_6()
+        {
+            //Arrange
+            double cathet1 = 1.2;
+            double cathet2 = 3.4;
+            double hypotenuse = 5.6;
+            double expectedResult = 2.04;
+
+            //Act
+            var result = RigthtTriangleFunctions.GetRigthTriangleArea(cathet1, cathet2, hypotenuse);
+
+            //Assert
+            Assert.AreEqual(expectedResult, result);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Should_throw_When_length_less_than_0()
+        {
+            //Arrange
+            double cathet1 = -1.2;
+            double cathet2 = 3.4;
+            double hypotenuse = 5.6;
+
+            //Act
+            var result = RigthtTriangleFunctions.GetRigthTriangleArea(cathet1, cathet2, hypotenuse);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Should_throw_When_length_too_large()
+        {
+            //Arrange
+            double cathet1 = double.MaxValue; 
+            double cathet2 = double.MaxValue;
+            double hypotenuse = double.MaxValue;
+
+            //Act
+            var result = RigthtTriangleFunctions.GetRigthTriangleArea(cathet1, cathet2, hypotenuse);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Should_throw_When_length_too_small()
+        {
+            //Arrange
+            double cathet1 = double.MinValue;
+            double cathet2 = double.MinValue;
+            double hypotenuse = double.MinValue;
+
+            //Act
+            var result = RigthtTriangleFunctions.GetRigthTriangleArea(cathet1, cathet2, hypotenuse);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Should_throw_When_length_is_NaN()
+        {
+            //Arrange
+            double cathet1 = double.NaN;
+            double cathet2 = 3;
+            double hypotenuse = 5;
+
+            //Act
+            var result = RigthtTriangleFunctions.GetRigthTriangleArea(cathet1, cathet2, hypotenuse);
+
+        }
+
+        [TestMethod] 
+        [ExpectedException(typeof(ArgumentOutOfRangeException))]
+        public void Should_throw_When_length_is_Infinity()
+        {
+            //Arrange
+            double cathet1 = 3;
+            double cathet2 = double.PositiveInfinity;
+            double hypotenuse = 5;
+
+            //Act
+            var result = RigthtTriangleFunctions.GetRigthTriangleArea(cathet1, cathet2, hypotenuse);
+
+        }
+    }
+}
---
+++ RigthTriangleFunctions.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+public static class RigthtTriangleFunctions
+{
+    /// <summary>
+    /// Получение площади прямоугольного треугольника
+    /// </summary>
+    /// <param name="cathetus1">катет</param>
+    /// <param name="cathetus2">катет</param>
+    /// <param name="hypotenuse">гипотенуза</param>
+    /// <returns>площадь прямоугольного треугольника</returns>
+    public static double GetRigthTriangleArea(double cathetus1, double cathetus2, double hypotenuse)
+    {
+        _VerificateInput(cathetus1);
+        _VerificateInput(cathetus2);
+        _VerificateInput(hypotenuse);
+
+        var area = _CalculateRigthTriangleArea(cathetus1, cathetus2);
+
+        _VerificateResult(area);
+
+        return area;
+    }
+
+    /// <summary>
+    /// Вычисление площади прямоугольного треугольника
+    /// </summary>
+    /// <param name="cathetus1">катет</param>
+    /// <param name="cathetus2">катет</param>
+    /// <returns>площадь прямоугольного треугольника</returns>
+    private static double _CalculateRigthTriangleArea(double cathetus1, double cathetus2)
+    {
+        var area = cathetus1 * cathetus2 / 2;
+        return area;
+    }
+
+    /// <summary>
+    /// Проверка входящих значений
+    /// </summary>
+    /// <param name="value">значение</param>
+    private static void _VerificateInput(double value)
+    {
+        if (value < 0)
+            throw new ArgumentOutOfRangeException("Длина стороны треугольника не может быть меньше 0.");
+
+        if (double.IsNaN(value) || double.IsInfinity(value))
+            throw new ArgumentOutOfRangeException("Длина стороны треугольника не является значимым числом.");
+    }
+
+    /// <summary>
+    /// Проверка результата
+    /// </summary>
+    /// <param name="value">значение</param>
+    private static void _VerificateResult(double value)
+    {
+        if (double.IsPositiveInfinity(value))
+            //todo При необходимости, можно использовать BigInteger для обработки больших значений.
+            throw new ArgumentOutOfRangeException("Слишком большое значение длины стороны треугольника.");
+
+        if (double.IsNegativeInfinity(value))
+            //todo При необходимости, можно использовать Decimal для обработки малых значений.
+            throw new ArgumentOutOfRangeException("Слишком маленькое значение длины стороны треугольника.");
+    }
+
+}
+
